1e83d5b7c78dad3445e2473b28bef97d6f896ff3
Levi Grubb <grubb.cts@gmail.com>
2025-11-03 16:25:23 -0600

    Learning strings and segfaults (oh boy\!)

--- strings.c
+++ strings.c
@@ -4,19 +4,22 @@
 int main() {
   char * immutable_string = "I am an immutable string";
   char changeable_string[] = "THIS CAN CHANGE";
+  char copy_of_i[25];
+  printf("Contents of immutable copy: %s", *copy_of_i);
 
-  printf("The string at memory location %p contains: %s\n", immutable_string);
-  printf("The string at memory location %p contains: %s\n", changeable_string);
-
+  printf("The string at memory location %p contains: %s\n", immutable_string, immutable_string);
+  printf("The string at memory location %p contains: %s\n", changeable_string, changeable_string);
+ 
   if (strncmp(immutable_string, changeable_string, 10) != 0) {
-    printf("when comparing '%s' and '%s', they are NOT the same", immutable_string, changeable_string);
+    printf("when comparing '%s' and '%s', they are NOT the same\n", immutable_string, changeable_string);
   } else if (strncmp(immutable_string, changeable_string, 10) == 0) {
-    printf("Match Found!");
+    printf("Match Found!\n");
   } else {
-    printf("Some crazy stuff happened");
+    printf("Some crazy stuff happened\n");
   }
   
   printf("The memory address, and content, of both strings, are show below:\n%p\n%p\n", immutable_string, changeable_string);
-  strncat(immutable_string, changeable_string, 20);
+  strncat(copy_of_i, immutable_string, sizeof(copy_of_i) - strlen(copy_of_i) - 1);
+  printf("%s\n", copy_of_i);
 }
 
